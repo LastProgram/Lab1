fddeb75ea49dd4ac91d47e2e506bea4f63d06933
LastProgram <145275888+LastProgram@users.noreply.github.com>
2024-11-08 00:11:57 +0700

    Add behavior

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.lab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val editText = findViewById<EditText>(R.id.edit_text)
+        val textView = findViewById<TextView>(R.id.text_view)
+
+        editText.setOnEditorActionListener { _, _, _ ->
+            val input = editText.text.toString()
+            if (input.equals("d", ignoreCase = true)) {
+                textView.text = "Это дуб!"
+            } else {
+                textView.text = "Это не дуб!"
+            }
+            true
+        }
     }
 }
